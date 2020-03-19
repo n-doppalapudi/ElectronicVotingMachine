ee2f5a2b70698bf5df7eb886d9791b9a2e36a8c7
Hitesh Bahar <42732631+hiteshbahar@users.noreply.github.com>
2020-03-19 09:39:43 -0700

    Added page linking fucntionality

--- action_page.php
+++ action_page.php
@@ -15,16 +15,21 @@ if(!empty($username)){
         // create connection
 
         $conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
-
+        // mysql_connect($host,$dbusername,$dbpassword);
+        // mysql_select_db($dbname);
 
 
         if(mysqli_connect_error()){
             die('Connection error ('. mysqli_connection_errno() .')'.mysqli_connection_error());
         }
         else{
+
             $sql = "select * from users where name = '$username' and pswd = '$password'";
             if($conn->query($sql)){
                 echo "Welcome ".$username;
+                echo '<script>
+                window.location="Page2.html";
+                </script>';
             }else{
                 echo "Error: ".$sql."<br>".$conn->error;
             }
